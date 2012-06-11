57a8c4c8fddd587e6f83ed9dd14fe87a2b2d4ec5
Josh Montoute <support@thinksquirrel.com>
2012-06-11 18:22:25 -0500

    EventHelper bugfixes

--- Assets/Editor/Thinksquirrel Common/Source/Common/EventHelper.cs
+++ Assets/Editor/Thinksquirrel Common/Source/Common/EventHelper.cs
@@ -28,11 +28,15 @@
 //
 using UnityEngine;
 using UnityEditor;
+using System.Collections.Generic;
 
 namespace ThinksquirrelSoftware.Common.Editor
 {
 	public static class EventHelper
 	{
+		private static HashSet<int> trackedMouseButtons = new HashSet<int>();
+		private static HashSet<KeyCode> trackedKeys = new HashSet<KeyCode>();
+		
 		public static bool LeftMouseDown
 		{
 			get { return IsMouseDown(0); }
@@ -41,6 +45,11 @@ namespace ThinksquirrelSoftware.Common.Editor
 		public static bool LeftMouseUp
 		{
 			get { return IsMouseUp(0); }	
+		}
+		
+		public static bool LeftMousePressed
+		{
+			get { return IsMousePressed(0); }	
 		}
 		
 		public static bool RightMouseDown
@@ -53,29 +62,89 @@ namespace ThinksquirrelSoftware.Common.Editor
 			get { return IsMouseUp(1); }	
 		}
 		
+		public static bool RightMousePressed
+		{
+			get { return IsMousePressed(1); }	
+		}
+		
 		public static bool IsMouseDown(int button)
 		{
-			return Event.current.type == EventType.MouseDown && Event.current.button == button;
+			bool down = Event.current.type == EventType.MouseDown && Event.current.button == button;
+			
+			if (down && !trackedMouseButtons.Contains(button))
+				trackedMouseButtons.Add(button);
+				
+			return down;
 		}
 		
 		public static bool IsMouseUp(int button)
 		{
-			return Event.current.type == EventType.MouseUp && Event.current.button == button;
+			bool up = Event.current.type == EventType.MouseUp && Event.current.button == button;
+			
+			if (up && trackedMouseButtons.Contains(button))
+				trackedMouseButtons.Remove(button);
+				
+			return up;
+		}
+		
+		public static bool IsMousePressed(int button)
+		{
+			return trackedMouseButtons.Contains(button);
+		}
+		
+		public static bool DeleteKeyDown
+		{
+			get { return IsKeyDown(KeyCode.Delete) || IsKeyDown(KeyCode.Backspace); }
+		}
+		
+		public static bool EnterKeyDown
+		{
+			get { return IsKeyDown(KeyCode.Return) || IsKeyDown(KeyCode.KeypadEnter); }
+		}
+		
+		public static bool DeleteKeyUp
+		{
+			get { return IsKeyUp(KeyCode.Delete) || IsKeyUp(KeyCode.Backspace); }
+		}
+
+		public static bool EnterKeyUp
+		{
+			get { return IsKeyUp(KeyCode.Return) || IsKeyUp(KeyCode.KeypadEnter); }
 		}
 		
 		public static bool DeleteKeyPressed
 		{
 			get { return IsKeyPressed(KeyCode.Delete) || IsKeyPressed(KeyCode.Backspace); }
 		}
-		
+
 		public static bool EnterKeyPressed
 		{
 			get { return IsKeyPressed(KeyCode.Return) || IsKeyPressed(KeyCode.KeypadEnter); }
 		}
+
+		public static bool IsKeyDown(KeyCode keyCode)
+		{
+			bool down = Event.current.type == EventType.KeyDown && Event.current.keyCode == keyCode;
+			
+			if (down && !trackedKeys.Contains(keyCode))
+				trackedKeys.Add(keyCode);
+			
+			return down;
+		}
+		
+		public static bool IsKeyUp(KeyCode keyCode)
+		{
+			bool up = Event.current.type == EventType.keyUp && Event.current.keyCode == keyCode;
+			
+			if (up && trackedKeys.Contains(keyCode))
+				trackedKeys.Remove(keyCode);
+			
+			return up;
+		}
 		
 		public static bool IsKeyPressed(KeyCode keyCode)
 		{
-			return Event.current.type == EventType.keyDown && Event.current.keyCode == keyCode;
+			return trackedKeys.Contains(keyCode);
 		}
 		
 		public static bool ShiftPressed
@@ -85,7 +154,7 @@ namespace ThinksquirrelSoftware.Common.Editor
 		
 		public static bool AltPressed
 		{
-			get { return IsModifierPressed(EventModifiers.Command); }
+			get { return IsModifierPressed(EventModifiers.Alt); }
 		}
 		
 		public static bool CtrlPressed
