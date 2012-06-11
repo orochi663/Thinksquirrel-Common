4e0311166699457becc35343231f464311b6ed98
Josh Montoute <support@thinksquirrel.com>
2012-06-11 16:01:06 -0500

    Added math helper class

---
+++ Assets/Plugins/Thinksquirrel Common/Source/Math/MathHelper.cs
@@ -0,0 +1,46 @@
+// Enumerable extension methods
+// Enumerable.cs
+// Thinksquirrel Software Common Libraries
+//  
+// Author:
+//       Josh Montoute <josh@thinksquirrel.com>
+// 
+// Copyright (c) 2011-2012, Thinksquirrel Software, LLC
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification, 
+// are permitted provided that the following conditions are met:
+//
+// Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+// SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT 
+// OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+// EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// This file is available at https://github.com/Thinksquirrel-Software/Thinksquirrel-Common
+//
+namespace ThinksquirrelSoftware.Common.Math
+{
+	public static class MathHelper
+	{
+		public static float MinMax(params float[] values)
+		{
+			float min = float.MaxValue, max = float.MinValue;
+			
+			foreach(float f in values)
+			{
+				min = System.Math.Min(f, min);
+				max = System.Math.Max(f, max);
+			}
+			
+			return System.Math.Abs(min) > System.Math.Abs(max) ? min : max;
+		}
+	}
+}
