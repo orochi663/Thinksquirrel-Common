726262d55bdfe793d29d86b7bc6e80a883f294c1
Josh Montoute <support@thinksquirrel.com>
2012-09-26 22:00:15 -0700

    Updated build scripts: 	* Removed erroneous define 	* Added -optimize flag 	* Added license/author information

--- build-compact-osx.pl
+++ build-compact-osx.pl
@@ -1,7 +1,34 @@
 #!/usr/bin/perl
 
-# Thinksquirrel Common Library build script for Unity projects (OSX)
-# Place this at the project root.
+# Thinksquirrel Common Library build script (OSX)
+# build-compact-osx.pl
+# Thinksquirrel Software Common Libraries
+#  
+# Authors:
+#       Josh Montoute <josh@thinksquirrel.com>
+#		Emil "AngryAnt" Johansen <Twitter: @AngryAnt>
+#
+# Original code adapted from the blog post here:
+# http://angryant.com/2011/02/02/assembling-and-assimilating/
+#
+# Copyright (c) 2011-2012, Thinksquirrel Software, LLC
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification, 
+# are permitted provided that the following conditions are met:
+#
+# Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+# SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT 
+# OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 use strict;
 use Getopt::Long;
@@ -40,5 +67,5 @@ sub BuildAssembly
 	print color ("reset");
 }
 
-BuildAssembly ("library", "'bin/compact/Plugins/Thinksquirrel Common/ThinksquirrelCommon.Runtime.dll'", $sourceThinksquirrelCommon, "-v -recurse:$sourceThinksquirrelCommon -d:RUNTIME $debugOptions -r:$assemblyUnityEngine");
-BuildAssembly ("library", "'bin/compact/Editor/Thinksquirrel Common/ThinksquirrelCommon.Editor.dll'", $sourceThinksquirrelEditor, "-v -recurse:$sourceThinksquirrelEditor -d:RUNTIME $debugOptions -r:'bin/compact/Plugins/Thinksquirrel Common/ThinksquirrelCommon.Runtime.dll',$assemblyUnityEngine,$assemblyUnityEditor");
+BuildAssembly ("library", "'bin/compact/Plugins/Thinksquirrel Common/ThinksquirrelCommon.Runtime.dll'", $sourceThinksquirrelCommon, "-v -recurse:$sourceThinksquirrelCommon -d:$debugOptions -r:$assemblyUnityEngine");
+BuildAssembly ("library", "'bin/compact/Editor/Thinksquirrel Common/ThinksquirrelCommon.Editor.dll'", $sourceThinksquirrelEditor, "-v -recurse:$sourceThinksquirrelEditor -d:$debugOptions -r:'bin/compact/Plugins/Thinksquirrel Common/ThinksquirrelCommon.Runtime.dll',$assemblyUnityEngine,$assemblyUnityEditor");
